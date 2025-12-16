c3ee9c57c3df7f2f43252ab70b927a4241ef563e
Felipe Maza <felipe@felipem.com>
2025-12-16 18:04:11 +0100

    corregido obtener ahora, obsoleto
    DeprecationWarning: datetime.utcnow() is deprecated

--- src/templateapi/api/demo_auth.py
+++ src/templateapi/api/demo_auth.py
@@ -14,7 +14,7 @@ from fastapi.openapi.utils import get_openapi
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 from typing import Optional
 
 # Configuración de seguridad
@@ -100,9 +100,9 @@ def authenticate_user(fake_db, username: str, password: str):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(UTC) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(UTC) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
