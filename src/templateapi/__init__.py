48a38e69be8f14f27179e4dcf3f47464682d78ce
Felipe Maza <felipe@felipem.com>
2025-12-16 18:07:01 +0100

    cambiado version del __init__ al pyproject

--- src/templateapi/__init__.py
+++ src/templateapi/__init__.py
@@ -1,6 +1,13 @@
 """Template"""
 
-__version__ = "0.2.0"
+import toml
+
+with open("pyproject.toml") as f:
+    config = toml.load(f)
+
+version = config["project"]["version"]
+
+__version__ = version
 
 from dotenv import load_dotenv
 
