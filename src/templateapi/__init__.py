a675ab2683a901637d926c4733f828aaf35b6f95
Felipe Maza <felipe@felipem.com>
2025-12-17 11:12:39 +0100

    Comprobamos si existen las variables de entorno que necesita la API.

--- src/templateapi/__init__.py
+++ src/templateapi/__init__.py
@@ -1,7 +1,11 @@
 """Template"""
 
+import os
+import logging
 import toml
+from dotenv import load_dotenv
 
+# Versión del paquete
 with open("pyproject.toml") as f:
     config = toml.load(f)
 
@@ -9,6 +13,17 @@ version = config["project"]["version"]
 
 __version__ = version
 
-from dotenv import load_dotenv
 
+# Cargamos variables de entorno y verificamos su existencia
 load_dotenv()
+
+env_vars = ["ENVIRONMENT", "LOG_DIR"]
+no_env_vars = []
+for ev in env_vars:
+    if os.getenv(ev) is None:
+        logging.error("No se ha encontrado la variable de entorno %s", ev)
+        no_env_vars.append(ev)
+if len(no_env_vars):
+    raise Exception(
+        f"ERROR: No se ha encontrado la variable de entorno {','.join(no_env_vars)} "
+    )
